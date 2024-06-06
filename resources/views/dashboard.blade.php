80dba9d9f431e8b4691e9f46ba8e643752e56ed4
CaduMarques005 <cadugoleiro12@gmail.com>
2024-06-06 11:52:16 -0300

    creating the front end of our form

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,11 +7,40 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
+
+
+                <form action="{{ route('question.store') }}" method="POST">
+                    @csrf
+                    <div class="mb-4">
+
+                    <label for="question" class="block mb-2 text-sm font-medium text-gray-900 dark:text-black">
+                        Question
+                    </label>
+
+                    <textarea
+                        id="question"
+                        name="question"
+                        rows="4"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                              placeholder="Ask me anything...">
+
+                        {{ old('question') }}
+                    </textarea>
+                        @error('question')
+                        <span class="text-red">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <button type="submit" class="mr-3 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        Save
+                    </button>
+
+                    <button type="reset" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                        Reset
+                    </button>
+
+
+                </form>
         </div>
     </div>
 </x-app-layout>
